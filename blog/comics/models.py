acef2b1f9af13709e909cdf124c7f3b14f951b29
Wanda Alberini <alberini.dssy@gmail.com>
2023-08-13 23:55:56 -0300

    agregado models.py de mi App comics

--- blog/comics/models.py
+++ blog/comics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=256)
+    subtitulo = models.CharField(max_length=256)
+    cuerpo = models.CharField(max_length=1500)
+    autor = models.CharField(max_length=256)
+    fecha_publicacion = models.DateField(null=True)
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+  
+    def __str__(self):
+        return f"{self.titulo}, {self.autor}, {self.fecha_publicacion}"
